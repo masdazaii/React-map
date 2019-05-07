55160759b3911ace340c4da33ad2f1b6bf0752e3
masdazaii <anggitprayoga@mail.ugm.ac.id>
2019-05-07 09:34:22 +0700

    API + map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PrimarySearchAppBar from './PrimarySearchAppBar';
+import Cards from './Cards';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    open: false,
+    favorite: 'default',
+    hotels: [
+      {nama: 'Aston Imperium',tanggal: '23 April 2019',deskripsi: 'nobar avenger:endgame'},
+      {nama: 'Aston Imperium',tanggal: '23 April 2019',deskripsi: 'nobar avenger:endgame'},
+      {nama: 'Aston Imperium',tanggal: '23 April 2019',deskripsi: 'nobar avenger:endgame'},
+      {nama: 'Aston Imperium',tanggal: '23 April 2019',deskripsi: 'nobar avenger:endgame'},
+      {nama: 'Aston Imperium',tanggal: '23 April 2019',deskripsi: 'nobar avenger:endgame'},
+    ]
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <PrimarySearchAppBar/>
+        <div className="row">
+          <div className="col 15">
+            {this.state.hotels.map((data,key)=>{
+              return (
+                <div>
+                  <Cards key={key} title={data.nama} tanggal={data.tanggal}/>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     );
   }
